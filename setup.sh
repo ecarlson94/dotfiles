5fb6dc8d9fcd4de7c9f61b4285ea63fb9b04476d
Eric Carlson <e.carlson94@gmail.com>
2020-03-05 10:55:46 -0700

    add setup file

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+ln -s ~/Documents/gitrepos/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/Documents/gitrepos/dotfiles/.vimrc ~/.vimrc
+ln -s ~/Documents/gitrepos/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/Documents/gitrepos/dotfiles/.zshrc ~/.zshrc
+ln -s ~/Documents/gitrepos/dotfiles/.zshrc.pre-oh-my-zsh ~/.zshrc.pre-oh-my-zsh
