b8d95533b28cfd3919c3806c414cb43a08f0c4cd
Eric Carlson <e.carlson94@gmail.com>
2020-03-05 11:57:14 -0700

    install plugins

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ ln -s ~/Documents/gitrepos/dotfiles/.vimrc ~/.vimrc
 ln -s ~/Documents/gitrepos/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/Documents/gitrepos/dotfiles/.zshrc ~/.zshrc
 ln -s ~/Documents/gitrepos/dotfiles/.zshrc.pre-oh-my-zsh ~/.zshrc.pre-oh-my-zsh
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-docker.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-docker
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
