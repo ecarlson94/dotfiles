c106c3b8b3436a76ce7d4a3b2e66ed3c9698f826
Eric Carlson <e.carlson94@gmail.com>
2022-02-16 16:13:57 -0700

    replace zplug with sheldon

--- zsh/custom/plugins.zsh
+++ zsh/custom/plugins.zsh
@@ -2,89 +2,67 @@
 #                                   Plugins
 # =============================================================================
 # Path to your oh-my-zsh installation.
-export ZSH=".zsh-plugins/ohmyzsh"
+export ZSH="$HOME/.sheldon/repos/github.com/ohmyzsh/ohmyzsh"
 
-# Check if zplug is installed
-if [[ ! -d ~/.zplug ]]; then
-    git clone https://github.com/zplug/zplug ~/.zplug
-    source ~/.zplug/init.zsh && zplug update
-    zplug "zplug/zplug", hook-build:"zplug --self-manage"
-fi
-source ~/.zplug/init.zsh
-
-# oh-my-zsh
-zplug "robbyrussell/oh-my-zsh", use:"lib/*.zsh"
-
-# Load theme
-zplug "mafredri/zsh-async", from:github, use:async.zsh
-zplug "romkatv/powerlevel10k", use:powerlevel10k.zsh-theme, from:github, at:next, as:theme, depth:1
-
-zplug "chrissicool/zsh-256color"
-
-# enhancedcd
-zplug "b4b4r07/enhancd", use:init.sh
-
-# enhanced history
-zplug "b4b4r07/zsh-history-ltsv", use:init.sh
-
-# Miscellaneous commands
-# zplug "zdharma/zsh-diff-so-fancy" TODO find replacement
-zplug "peco/peco", as:command, from:gh-r, use:"*${(L)$(uname -s)}*amd64*"
-
-# Bookmarks and jump
-zplug "jocelynmallon/zshmarks"
-
-zplug "plugins/colored-man-pages", from:oh-my-zsh
-zplug "plugins/common-aliases",    from:oh-my-zsh
-zplug "plugins/command-not-found", from:oh-my-zsh
-zplug "plugins/copydir",           from:oh-my-zsh
-zplug "plugins/copyfile",          from:oh-my-zsh
-zplug "plugins/cp",                from:oh-my-zsh
-zplug "plugins/dircycle",          from:oh-my-zsh
-zplug "plugins/encode64",          from:oh-my-zsh
-zplug "plugins/extract",           from:oh-my-zsh
-zplug "plugins/fancy-ctrl-z",      from:oh-my-zsh
-zplug "plugins/history",           from:oh-my-zsh
-zplug "plugins/urltools",          from:oh-my-zsh
-zplug "plugins/web-search",        from:oh-my-zsh
-zplug "plugins/z",                 from:oh-my-zsh
-
-# Supports oh-my-zsh plugins and the like
-if [[ $OSTYPE = (linux)* ]]; then
-    zplug "plugins/archlinux",     from:oh-my-zsh, if:"(( $+commands[pacman] ))"
-    zplug "plugins/dnf",           from:oh-my-zsh, if:"(( $+commands[dnf] ))"
-    zplug "plugins/mock",          from:oh-my-zsh, if:"(( $+commands[mock] ))"
+# Check if sheldon is installed
+if [[ ! -f ~/.local/bin/sheldon ]]; then
+    curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh \
+        | bash -s -- --repo rossmacarthur/sheldon --to ~/.local/bin
 fi
 
-if [[ $OSTYPE = (darwin)* ]]; then
-    zplug "lib/clipboard",         from:oh-my-zsh
-    zplug "plugins/osx",           from:oh-my-zsh
-    zplug "plugins/macports",      from:oh-my-zsh, if:"(( $+commands[port] ))"
-fi
+# "zsh-users/zsh-history-substring-search"
+zmodload zsh/terminfo
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
+bindkey "^[[1;5A" history-substring-search-up
+bindkey "^[[1;5B" history-substring-search-down
 
-zplug "plugins/brew",           from:oh-my-zsh, if:"(( $+commands[brew] ))"
-zplug "plugins/dotnet",         from:oh-my-zsh, if:"(( $+commands[dotnet] ))"
-zplug "plugins/git",            from:oh-my-zsh, if:"(( $+commands[git] ))"
-zplug "plugins/golang",         from:oh-my-zsh, if:"(( $+commands[go] ))"
-zplug "plugins/node",           from:oh-my-zsh, if:"(( $+commands[node] ))"
-zplug "plugins/npm",            from:oh-my-zsh, if:"(( $+commands[npm] ))"
-zplug "plugins/pip",            from:oh-my-zsh, if:"(( $+commands[pip] ))"
-zplug "plugins/sudo",           from:oh-my-zsh, if:"(( $+commands[sudo] ))"
-zplug "plugins/gpg-agent",      from:oh-my-zsh, if:"(( $+commands[gpg-agent] ))"
-zplug "plugins/ssh-agent",      from:oh-my-zsh, if:"(( $+commands[ssh-agent] ))"
-zplug "plugins/systemd",        from:oh-my-zsh, if:"(( $+commands[systemctl] ))"
-zplug "plugins/docker",         from:oh-my-zsh, if:"(( $+commands[docker] ))"
-zplug "plugins/docker-compose", from:oh-my-zsh, if:"(( $+commands[docker-compose] ))"
-zplug "plugins/tmux",           from:oh-my-zsh, if:"(( $+commands[tmux] ))"
-zplug "plugins/tmuxinator",     from:oh-my-zsh, if:"(( $+commands[tmuxinator] ))"
-zplug "plugins/terraform",      from:oh-my-zsh, if:"(( $+commands[terraform] ))"
+# "zsh-users/zsh-syntax-highlighting"
+typeset -gA ZSH_HIGHLIGHT_STYLES ZSH_HIGHLIGHT_PATTERNS
 
-zplug "djui/alias-tips"
-zplug "hlissner/zsh-autopair", defer:2
-zplug "zsh-users/zsh-completions"
-zplug "zsh-users/zsh-autosuggestions"
+ZSH_HIGHLIGHT_STYLES[cursor]=fg=yellow,bold
+ZSH_HIGHLIGHT_STYLES[default]=none
+ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=green,bold
+ZSH_HIGHLIGHT_STYLES[reserved-word]=fg=green,bold
+ZSH_HIGHLIGHT_STYLES[alias]=fg=cyan,bold
+ZSH_HIGHLIGHT_STYLES[builtin]=fg=cyan,bold
+ZSH_HIGHLIGHT_STYLES[function]=fg=cyan,bold
+ZSH_HIGHLIGHT_STYLES[command]=fg=white,bold
+ZSH_HIGHLIGHT_STYLES[precommand]=fg=white,underline
+ZSH_HIGHLIGHT_STYLES[commandseparator]=none
+ZSH_HIGHLIGHT_STYLES[hashed-command]=fg=green,bold
+ZSH_HIGHLIGHT_STYLES[path]=fg=214,underline
+ZSH_HIGHLIGHT_STYLES[globbing]=fg=063
+ZSH_HIGHLIGHT_STYLES[history-expansion]=fg=white,underline
+ZSH_HIGHLIGHT_STYLES[single-hyphen-option]=fg=070
+ZSH_HIGHLIGHT_STYLES[double-hyphen-option]=fg=070
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument]=none
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=063
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=063
+ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]=fg=009
+ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]=fg=009
+ZSH_HIGHLIGHT_STYLES[assign]=none
 
-# zsh-syntax-highlighting must be loaded after executing compinit command
-# and sourcing other plugins
-zplug "zsh-users/zsh-syntax-highlighting", defer:2
-zplug "zsh-users/zsh-history-substring-search", defer:3
+ZSH_HIGHLIGHT_PATTERNS=('rm -rf *' 'fg=white,bold,bg=red')
+
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor line)
+
+# "zsh-users/zsh-autosuggestions"
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=075'
+
+# "djui/alias-tips"
+ZSH_PLUGINS_ALIAS_TIPS_EXCLUDES="(_ ll vi)"
+ZSH_PLUGINS_ALIAS_TIPS_FORCE=1
+
+# ZSH_PLUGINS_ALIAS_TIPS_REVEAL=1
+ZSH_PLUGINS_ALIAS_TIPS_REVEAL_EXCLUDES="(_ ll vi)"
+
+# "b4b4r07/enhancd"
+ENHANCD_COMPLETION_BEHAVIOR="list"
+ENHANCD_HOOK_AFTER_CD="ls -CF"
+
+# "b4b4r07/zsh-history-ltsv"
+ZSH_HISTORY_KEYBIND_GET_BY_DIR="^r"
+# ZSH_HISTORY_KEYBIND_GET_ALL="^r^a"
+
+eval "$(sheldon source)"
