0f88432633ae13e3b0fa714113667700805990e9
Eric Carlson <e.carlson94@gmail.com>
2023-10-20 10:53:30 -0400

    configure lualine theme to catppuccin

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,6 +2,6 @@ return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
   opts = function(_, opts)
-    table.insert(opts, { theme = "nord" })
+    table.insert(opts, { theme = "catppuccin" })
   end,
 }
