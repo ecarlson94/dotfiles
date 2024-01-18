7087f7eafc16457d042165694e5ad3e29728779e
Eric Carlson <e.carlson94@gmail.com>
2024-01-18 14:36:27 -0500

    update shell formatter to use spaces for tabs

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -22,6 +22,7 @@ vim.opt.conceallevel = 0
 vim.opt.relativenumber = false
 
 -- Whitespace {{{
+vim.opt.list = true -- Display whitespace
 vim.opt.listchars = {
   space = "·",
   nbsp = "·",
@@ -31,7 +32,12 @@ vim.opt.listchars = {
   trail = "~",
   eol = "¬",
 }
-vim.opt.list = true -- Display whitespace
+
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.autoindent = true
+vim.o.tabstop = 2 -- A TAB character looks like 4 spaces
+vim.o.softtabstop = 2 -- A TAB character looks like 4 spaces
+vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
 
 vim.cmd([[autocmd BufWritePre * :%s/\s\+$//e]]) -- Automatically remove trailing white space on save
 -- }}}
